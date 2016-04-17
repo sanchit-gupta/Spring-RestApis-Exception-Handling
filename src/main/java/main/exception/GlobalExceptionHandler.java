c2bc0838de6e6497f8ce82cf8bc22f5ece3c85bf
sanchit <sanchit@intelligrape.com>
2016-04-17 19:29:31 +0530

    throw an Exception

--- src/main/java/main/exception/GlobalExceptionHandler.java
+++ src/main/java/main/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ package main.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -14,30 +13,27 @@ import java.util.Map;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = CustomException.class)
-    public Map<String,Object> handleInvalidParamException(InvalidParamException e){
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("InvalidParamException",e.getMessage());
-        map.put("errorMsg ",e.getMessage());
+    public Map<String, Object> handleInvalidParamException(InvalidParamException e) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("InvalidParamException", e.getMessage());
+        map.put("errorMsg ", e.getMessage());
         map = addErrorCodeInResultMap(map);
         return map;
     }
 
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
-    public Map<String,Object> handleException(Exception e){
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("Error ",HttpStatus.INTERNAL_SERVER_ERROR.name());
-        map.put("errorMsg ",e.getMessage());
-        map.put("code","1");
+    public Map<String, Object> handleException(Exception e) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("Error ", HttpStatus.INTERNAL_SERVER_ERROR.name());
+        map.put("errorMsg ", e.getMessage());
+        map.put("code", "1");
         e.printStackTrace();
         return map;
     }
 
-    Map<String,Object> addErrorCodeInResultMap(Map<String,Object> map) {
-        map.put("code","0");
+    Map<String, Object> addErrorCodeInResultMap(Map<String, Object> map) {
+        map.put("code", "0");
         return map;
     }
 
--- src/main/java/main/service/ApiService.java
+++ src/main/java/main/service/ApiService.java
@@ -9,14 +9,10 @@ import java.util.Map;
 public class ApiService {
 
     public Map saveToDB(String id) {
-        System.out.println("from service id : "+id);
+        System.out.println("from service id : " + id);
 //        Map map = new HashMap();
-//
 //        map.put("id ",id);
 //        return map;
-        throw new InvalidParamException("Invalid params found as id : "+id);
-//        throw new IllegalStateException("IllegalStateException");
-
+        throw new InvalidParamException("Invalid params found as id : " + id);
     }
-
 }
